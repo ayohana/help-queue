e69e69a3c6f2a19efb5b21d4afd39cda476d941b
Adela Darmansyah <adela.yohana@gmail.com>
2020-05-05 17:49:52 -0700

    Add Firebase Hosting and Styled Components library

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from 'styled-components';
+
+const HelpQueueHeader = styled.h1`
+  font-size: 24px;
+  text-align: center;
+  color: white;
+`;
+
+const StyledWrapper = styled.section`
+  background-color: orange;
+`;
 
 function Header(){
   return (
-    <React.Fragment>
-      <h1> Help Queue</h1>
+    <StyledWrapper>
+      <HelpQueueHeader>
+        Help Queue
+      </HelpQueueHeader>
       <ul>
         <li>
           <Link to="/">Home</Link>
@@ -13,7 +26,7 @@ function Header(){
           <Link to="/signin">Sign In</Link>
         </li>
       </ul>
-    </React.Fragment>
+    </StyledWrapper>
   );
 }
 
